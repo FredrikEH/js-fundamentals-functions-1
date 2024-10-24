52417541cf995f0c5506e9aa4e2217d235405f2d
Fredrik Haupt <fredrik.haupt@se.experis.com>
2024-10-24 10:44:43 +0200

    commit

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,9 +7,17 @@
 // 2      | 3
 //
 // TODO: write code below
+
+//My solution
 function increment(n){
   return ++n
 }
+
+//Ai solution
+function increment(num) {
+  return num + 1;
+}
+
 // 2. Define a function that capitalises any string
 
 // Example Input and Output:
@@ -20,12 +28,23 @@ function increment(n){
 // Hello | Hello
 
 // TODO: write code below
+
+//My solution
 function capitalise(s){
   if(typeof s !== 'string'){
     return s
   }
   return s.charAt(0).toUpperCase() + s.slice(1)
 }
+
+//AI solution
+function capitalize(str) {
+  if (str.length === 0) {
+      return str;
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
 //
@@ -36,12 +55,28 @@ function capitalise(s){
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+
+//My solution
 function smiley(s){
   if(typeof s !== 'string'){
     return s
   }
   return 'Hi, ' + capitalise(s) + ' :)'
 }
+
+//AI solution
+function capitalize(str) {
+  if (str.length === 0) {
+      return str;
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function greetWithSmiley(name) {
+  const capitalizedName = capitalize(name);
+  return `Hi, ${capitalizedName} :)`;
+}
+
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
 // Example Input and Output:
@@ -53,6 +88,8 @@ function smiley(s){
 // [1, 'edward']        | 1
 //
 // TODO: write code below
+
+//My solution
 function stringAmount(a){
   let count = 0
   for(let i = 0; i < a.length; i++){
@@ -62,6 +99,18 @@ function stringAmount(a){
   }
   return count
 }
+
+//AI solution
+function countStrings(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] === 'string') {
+          count++;
+      }
+  }
+  return count;
+}
+
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
 // Example Input and Output:
@@ -72,17 +121,40 @@ function stringAmount(a){
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
+
+//My solution
 function edward(o){
   if(!o.hasOwnProperty('edward')){
     o.edward = 'amazing'
   }
   return o
 }
+
+//AI solution
+function addEdwardProperty(obj) {
+  if (!obj.hasOwnProperty('edward')) {
+      obj.edward = 'amazing';
+  }
+  return obj;
+}
+
 // TODO: change each undefined below to be the name of the functions you defined
+/*
+//My solution
 module.exports = {
   a: increment, // 1. change undefined to be the name of the function you defined for the first TODO
   b: capitalise, // 2. change undefined to be the name of the function you defined for the second TODO)
   c: smiley, // etc
   d: stringAmount,
   e: edward
+}
+  */
+
+//AI solution
+module.exports = {
+  a: increment, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: capitalize, // 2. change undefined to be the name of the function you defined for the second TODO
+  c: greetWithSmiley, // etc
+  d: countStrings,
+  e: addEdwardProperty
 }
--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,12 +9,23 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+
+//My solution
 function between(one, two){
   let array = []
   for(let i = one; i <= two; i++){
     array.push(i)
   }
   return array
+}
+
+//AI solution
+function range(lower, upper) {
+  let result = [];
+  for (let i = lower; i <= upper; i++) {
+      result.push(i);
+  }
+  return result;
 }
 
 // 2. define a function that takes two arguments: a string and a number.
@@ -28,6 +39,8 @@ function between(one, two){
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+
+//My solution
 function shout(word, amount){
   let result = ''
   result += word.toUpperCase()
@@ -35,6 +48,11 @@ function shout(word, amount){
     result += '!'
   }
   return result
+}
+
+//AI solution
+function emphasize(str, num) {
+  return str.toUpperCase() + '!'.repeat(num);
 }
 
 // 3. define a function that takes two arguments: a string and a number.
@@ -48,6 +66,8 @@ function shout(word, amount){
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
 // TODO: write code below
+
+//My solution
 function clock(time, extra){
   let array = time.split(':')
   let total = parseInt(array[0]) * 60 + parseInt(array[1]) + extra
@@ -62,9 +82,32 @@ function clock(time, extra){
   return hour + ':' + minute 
 }
 
+//AI solution
+function addMinutes(timeStr, minutesToAdd) {
+  let [hours, minutes] = timeStr.split(':').map(Number);
+  let totalMinutes = hours * 60 + minutes + minutesToAdd;
+  let newHours = Math.floor(totalMinutes / 60) % 24;
+  let newMinutes = totalMinutes % 60;
+
+  let formattedHours = newHours === 0 ? '00' : newHours.toString(); // Do not pad hours with leading zero except for '00'
+  let formattedMinutes = newMinutes.toString().padStart(2, '0');
+
+  return `${formattedHours}:${formattedMinutes}`;
+}
+
 // TODO: change the exported value to be the name of the function you defined
+/*
+//My solution
 module.exports = {
   a: between, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
   b: shout, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
   c: clock // etc
+}
+*/
+
+//AI solution
+module.exports = {
+  a: range, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: emphasize, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: addMinutes // etc
 }
